e10242a7d2633740f602c05795e72d7ee3b5a930
Ozioma7 <okonisaozioma@gmail.com>
2023-10-05 11:39:55 +0000

    advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/\[SENDER: (.*?)\] \[RECEIVER: (.*?)\] \[FLAGS: (.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from: (.*?)\] \[to: (.*?)\] \[flags: (.*?)\]/).join(",")
