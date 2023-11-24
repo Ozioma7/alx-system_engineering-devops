c53a90957a0bd3071b8cdeb882ea456f921f4168
Ozioma7 <okonisaozioma@gmail.com>
2023-11-24 18:08:21 +0000

    tasks 1 and 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
-# Kills the process killmenow
+# Kills a process name killmenow
 
-exec  {  'pkill killmenow':
-  path      =>  '/usr/bin',
-  command   =>  'pkill killmenow',
-  provider  =>  'shell',
-  returns   =>  [0, 1]
+exec { 'pkill killmenow':
+  path     => '/usr/bin',
+  command  => 'pkill killmenow',
+  provider => shell,
+  returns  => [0, 1]
 }
